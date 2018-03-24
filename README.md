b70b523e8060046ce5b961331847a24abdf9f9ad
ropaolle <ropaolle@gmail.com>
2018-03-24 15:44:13 +0100

    Second footer

--- README.md
+++ README.md
@@ -33,3 +33,20 @@ https://github.com/billerickson/display-posts-shortcode/blob/master/README.md#pa
 
 ## Shortcodes
 [display-posts include_title="false" posts_per_page="20" include_content="true" wrapper="div" include_date="true" date_format="l \\d\\e\\n d F"]
+
+Rosengårdens Restaurang
+
+Vallentuna Mat & Catering
+Rosengårdsvägen XX
+XXX XX VALLENTUNA
+
+Tel: 08-6667788
+Epost: kontakt@vallentuna.se
+
+
+</p>
+
+<p>Köksmästare: Andreas Rosén</p>
+
+<h2 class="widget-title">Öppettider</h2>
+Lunch: Mån–Fre: 10:00 – 14:00
--- themes/rosengarden/functions.php
+++ themes/rosengarden/functions.php
@@ -44,3 +44,22 @@ function displayweek(){
     add_shortcode('week', 'displayweek');
     
     
+
+/**
+ * Initializes themes widgets.
+ */
+function rosengarden_widgets_init() {
+    
+    register_sidebar( array(
+        'name'          => __( 'Footer Full Second', 'rosengarden' ),
+        'id'            => 'footerfull-second',
+        'description'   => 'Widget area below main content and above footer',
+        'before_widget'  => '<div id="%1$s" class="footer-widget %2$s '. 'olle' .'">', 
+        'after_widget'   => '</div><!-- .footer-widget -->', 
+        'before_title'   => '<h3 class="widget-title">', 
+        'after_title'    => '</h3>', 
+    ) );
+
+}
+
+add_action( 'widgets_init', 'rosengarden_widgets_init' );
