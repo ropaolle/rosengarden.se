55a4d40bdafa4bfb157bc5a97b55df207df3ac57
ropaolle <ropaolle@gmail.com>
2018-03-25 08:06:56 +0200

    Custom bottom footer

--- themes/rosengarden/footer.php
+++ themes/rosengarden/footer.php
@@ -26,19 +26,9 @@ $container = get_theme_mod( 'understrap_container_type' );
 				<footer class="site-footer" id="colophon">
 
 					<div class="site-info">
-
-							<a href="<?php  echo esc_url( __( 'http://wordpress.org/','understrap' ) ); ?>"><?php printf( 
-							/* translators:*/
-							esc_html__( 'Proudly powered by %s', 'understrap' ),'WordPress' ); ?></a>
-								<span class="sep"> | </span>
-					
-							<?php printf( // WPCS: XSS ok.
-							/* translators:*/
-								esc_html__( 'Theme: %1$s by %2$s.', 'understrap' ), $the_theme->get( 'Name' ),  '<a href="'.esc_url( __('http://understrap.com', 'understrap')).'">understrap.com</a>' ); ?> 
-				
-							(<?php printf( // WPCS: XSS ok.
-							/* translators:*/
-								esc_html__( 'Version: %1$s', 'understrap' ), $the_theme->get( 'Version' ) ); ?>)
+							
+							<?php print( esc_html__( 'Vallentuna Mat & Catering, &copy; 2018', 'understrap' ) ); ?>
+							
 					</div><!-- .site-info -->
 
 				</footer><!-- #colophon -->
